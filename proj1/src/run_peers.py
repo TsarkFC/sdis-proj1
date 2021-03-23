b9cbba0b9d185e2f097efa51f89261692cff4933
TsarkFC <up201806779@fe.up.pt>
2021-03-23 10:29:57 +0000

    Killing children processes on run script

--- proj1/src/run_peers.py
+++ proj1/src/run_peers.py
@@ -1,5 +1,6 @@
 #python3 run_peers.py <peers_num>
 
+from signal import signal, SIGINT
 import subprocess, sys, os
 
 # java Peer <protocol_version> <peer_id> <service_access_point> <MC_addr> <MC_port> <MDB_addr> <MDB_port> <MDR_addr> <MDR_port>
@@ -11,19 +12,29 @@ mc_addr = ("228.25.25.25", "4445")
 mdb_addr = ("228.25.25.25", "4446")
 mdr_addr = ("228.25.25.25", "4447")
 
+def handler(signal_received, frame):
+    print('SIGINT or CTRL-C detected. Exiting gracefully')
+    os._exit(0)
+
 def peer():
     subprocess.run(["java", "-cp", ".", "Peer", "access"])
     #subprocess.run(["java", "Peer", "1.0", str(i), mc_addr[0], mc_addr[1], mdb_addr[0], mdb_addr[1], mdr_addr[0], mdr_addr[1]])
     os._exit(0)  
 
 def start():
-    subprocess.run(["fuser", "-k", "1099/tcp"])
-    subprocess.run("rmiregistry &", shell=True)
-    subprocess.run(["javac", "-cp", ".", "Peer.java"])
+	signal(SIGINT, handler)
+	subprocess.run(["fuser", "-k", "1099/tcp"])
+	subprocess.run("rmiregistry &", shell=True)
+	subprocess.run(["javac", "-cp", ".", "Peer.java"])
 
-    for i in range(peers_num):
-        newpid = os.fork()
-        if newpid == 0:
-            peer()
+	for i in range(peers_num):
+		newpid = os.fork()
+		if newpid == 0:
+			peer()
+
+	print('Running. Press CTRL-C to exit.')
+	while True:
+		# Do nothing and hog CPU forever until SIGINT received.
+		pass
 
 start()
